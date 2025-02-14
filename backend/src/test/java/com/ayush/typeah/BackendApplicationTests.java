a1e5ec5591357734bb8711c13eac9088cc962d23
dev-ayush101 <ayushmishraofficial.619@gmail.com>
2025-02-14 20:10:00 +0530

    Initial commit: Project skeleton

---
+++ backend/src/test/java/com/ayush/typeah/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayush.typeah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
